b7346de58396e663afcf2bf63a80b0b0924dee4c
coe <coe.app@gmail.com>
2021-03-25 15:37:50 +0900

    Initial Commit

---
+++ SwiftUIAdMob/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIAdMob
+//
+//  Created by 日向 強 on 2021/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIAdMob/SwiftUIAdMobApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAdMobApp.swift
+//  SwiftUIAdMob
+//
+//  Created by 日向 強 on 2021/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAdMobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
